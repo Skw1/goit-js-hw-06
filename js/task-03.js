4efc89657eb61236ad65eac4faac0a2a854f9dc6
Andrii Doluda <andrewdoluda@gmail.com>
2023-12-17 23:58:49 +0100

    Task 3

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,13 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const galleryAdd = document.querySelector('.gallery');
+
+const galleryItems = images.map(image => {
+
+  return `<li><img src="${image.url}" alt="${image.alt}"></li>`;
+
+});
+
+galleryAdd.insertAdjacentHTML('beforeend', galleryItems.join(''));
