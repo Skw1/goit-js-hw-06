03f8f79e86fd2b88a0bdb3d370e3713a48791315
Andrii Doluda <andrewdoluda@gmail.com>
2023-12-18 02:04:35 +0100

    Task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+ 
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', function (i) {
+  i.preventDefault();
+
+const formData = new FormData(loginForm);
+const userData = {};
+
+formData.forEach((value, key) => {
+
+    userData[key] = value;
+
+});
+
+if (!userData.email || !userData.password){
+
+    alert('! Please fill in all fields of the form !');
+
+} else {
+
+    console.log(userData);
+    loginForm.reset();
+  }
+});
